d2bcf648a3e3cd08896ff3f248219ac5fb990102
Ibrahim <IbrahimKoutabli@localhost.localdomain>
2018-06-05 17:03:55 +0100

    Some JSX restrictions, class and adjacent components

--- src/App.js
+++ src/App.js
@@ -3,19 +3,21 @@ import './App.css';
 
 class App extends Component {
   render() {
-    // return ( // This is gets compiled and is equivalent to:
-    //   <div className="App">
-    //  <h1>Hi, I'm a React App</h1>
-    //   </div>
-    // );
-  return React.createElement('div',{className:'App'},React.createElement('h1',null,'Hi I\'m a React App!'))// Equivalent to this
-  // takes at least 3 arguments, 
-  // first argument box type: component, div, h1, etc etc
-  // second arg is the configuration for this 
-  // Third argument is the number of children
-  // As we can see, using JSX can get quite ugly and its easier to use "HTML" Tags
-  // We see from the HTML that we have a className "App" that aligns the text to the center but it isn't being applied, the workaround is:
-  // 
+    return ( // This is gets compiled and is equivalent to the next return statement
+      
+      //in normal HTML we'd use the word class, but because this is JS we can't do that as its a reserved keyword. 
+      <div className="App">  
+     <h1>Hi, I'm a React App</h1>
+      </div>
+    );
+  // return React.createElement('div',{className:'App'},React.createElement('h1',null,'Hi I\'m a React App!'))// Equivalent to this
+  // // takes at least 3 arguments, 
+  // // first argument box type: component, div, h1, etc etc
+  // // second arg is the configuration for this 
+  // // Third argument is the number of children
+  // // As we can see, using JSX can get quite ugly and its easier to use "HTML" Tags
+  // // We see from the HTML that we have a className "App" that aligns the text to the center but it isn't being applied, the workaround is:
+  // // 
   }
 
 }
