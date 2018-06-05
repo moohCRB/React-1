e0ad66082daf54911c399ba6e37e570b27a9a289
Ibrahim <IbrahimKoutabli@localhost.localdomain>
2018-06-05 16:57:07 +0100

    Using CSS styled with React.createElement

--- src/App.js
+++ src/App.js
@@ -8,13 +8,14 @@ class App extends Component {
     //  <h1>Hi, I'm a React App</h1>
     //   </div>
     // );
-  return React.createElement('div',null,React.createElement('h1',null,'Hi I\'m a React App!'))// Equivalent to this
+  return React.createElement('div',{className:'App'},React.createElement('h1',null,'Hi I\'m a React App!'))// Equivalent to this
   // takes at least 3 arguments, 
   // first argument box type: component, div, h1, etc etc
   // second arg is the configuration for this 
   // Third argument is the number of children
   // As we can see, using JSX can get quite ugly and its easier to use "HTML" Tags
-  
+  // We see from the HTML that we have a className "App" that aligns the text to the center but it isn't being applied, the workaround is:
+  // 
   }
 
 }
