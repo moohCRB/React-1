7bea6902741910c79fc7a61eaa202d1cbe2c2080
IbrahimKoutabli <ibrahim.koutabli@gmail.com>
2018-06-14 11:32:52 +0100

    Transmformed the Person functional component to a stateful component for further investigation on Component Methods

--- src/Components/Persons/Person.js
+++ src/Components/Persons/Person.js
@@ -4,21 +4,24 @@
 
 // There's two ways to style components:
 // 
-import React from 'react';
+import React,{Component} from 'react';
 import './Person/Person.css'; // file extension must be included... file extensions can only be omitted for JS files
 
 
-const person = (props) => { 
-    return (
-    <div className="Person">
-
-        <p onClick={props.click}>My name is {props.name} and I am {props.age} years old!</p>
-        {/* Children are a special property given to us by react which is the text in between the <> </> tags that are passed into this component */}
-       <p> {props.children}</p> 
-    {props.enableInput&& <input type="text" onChange={props.changed} value={props.name}/>} {/*two way binding*/}
-       {/*onChange will be fired whenever the value of the input changes*/}
-        </div>
-    )
+class Person extends Component {
+    render(){
+      return (
+            <div className="Person">
+        
+                <p onClick={this.props.click}>My name is {this.props.name} and I am {this.props.age} years old!</p>
+                {/* Children are a special property given to us by react which is the text in between the <> </> tags that are passed into this component */}
+               <p> {this.props.children}</p> 
+            {this.props.enableInput&& <input type="text" onChange={this.props.changed} value={this.props.name}/>} {/*two way binding*/}
+               {/*onChange will be fired whenever the value of the input changes*/}
+                </div>
+            )
+    }
 }
+    
 
-export default person;
+export default Person;
