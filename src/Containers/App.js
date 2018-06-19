ddc5b35f9be316d51e47d1aad345896d4b95beb1
IbrahimKoutabli <ibrahim.koutabli@gmail.com>
2018-06-19 09:45:47 +0100

    Basic skeleton of the burger App

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from "react";
-import styled, { css } from "react-emotion";
+// import styled, { css } from "react-emotion";
+import Layout from "../Components/Layout/Layout";
+import BurgerBuilder from "./BurgerBuilder/BurgerBuilder";
 
-const Header = styled("div")`
-    width: 100%;
-    background-color: green;
-    height: 50px;
-    position: fixed;
-    z-index: 2;
-    top: 50px;
-    left: 0;
-    border-bottom: 1px solid black;
-`;
+// const Header = styled("div")`
+//   width: 100%;
+//   background-color: green;
+//   height: 50px;
+//   position: fixed;
+//   z-index: 2;
+//   top: 50px;
+//   left: 0;
+//   border-bottom: 1px solid black;
+// `;
 // const containerStyles = css`
-//     display: flex;
+//     display: flex; v
 //     flex-direction: column;
 //     width: 100%;
 //     height: 5rem;
@@ -21,16 +23,13 @@ const Header = styled("div")`
 
 class App extends Component {
   render() {
-    console.log("Yes");
-      return(
-    <div>
-          {/* <h1>Hi, I'm a React App</h1> */}
-   
-      <div>asdasdasd adsad asd sad sa</div>
-      <Header>asdsa</Header>
-    </div>
-    
-      )
+    return (
+      <div>
+        <Layout>
+         <BurgerBuilder />
+        </Layout>
+      </div>
+    );
   }
 }
 
