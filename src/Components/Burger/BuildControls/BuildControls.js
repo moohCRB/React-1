ce352e51b348b9bf440861bfbde01cefbcb22fc6
IbrahimKoutabli <ibrahim.koutabli@gmail.com>
2018-06-20 15:10:11 +0100

    Logic to handle adding/removing and disabling button

--- src/Components/Burger/BuildControls/BuildControls.js
+++ src/Components/Burger/BuildControls/BuildControls.js
@@ -23,7 +23,13 @@ const controls = [
 const buildControls = (props) => (
 <BuildControls>
 {controls.map( ctrl => (
-    <BuildControl key={ctrl.label} label={ctrl.label}/>
+    <BuildControl 
+    key={ctrl.label}
+    label={ctrl.label}
+    added = {()=>props.ingredientAdded(ctrl.type)}
+    removed = {()=>props.ingredientRemoved(ctrl.type)}
+    disabled={props.disabled[ctrl.type]}
+    />
 ))}
     </BuildControls>
 );
