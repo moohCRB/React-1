f4de7971400496573d1ef203fdd64790f8131e4a
IbrahimKoutabli <ibrahim.koutabli@gmail.com>
2018-06-20 13:09:35 +0100

    Added controls to the burgerBuilder

--- src/Containers/BurgerBuilder/BurgerBuilder.js
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Aux from "../../hoc/Aux"
 import Burger from "../../Components/Burger/Burger";
+import BuildControls from "../../Components/Burger/BuildControls/BuildControls";
 
 class BurgerBuilder extends Component{
 state = {
@@ -16,7 +17,7 @@ state = {
         return(
             <Aux>
                <Burger ingredients={this.state.ingredients}/>
-                <div>Build Controls</div>
+               <BuildControls />
                 </Aux>
         );
     }
