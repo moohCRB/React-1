3b57f62c460c4e8887160891514117b3c38fd377
IbrahimKoutabli <ibrahim.koutabli@gmail.com>
2018-06-28 16:43:48 +0100

    Added toolbar in the layout

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -2,13 +2,14 @@ import React from "react";
 import Aux from "../../hoc/Aux";
 import "./Layout.css";
 import styled from "react-emotion";
-
+import Toolbar from "../Navigation/Toolbar/Toolbar";
 const Header = styled("div")`
 margin-top: 50px;
 `;
 
 const layout = props => (
   <Aux>
+    <Toolbar />
     <div>Toolbar,SideDrawer,Backdrop</div>
     <main className={"Content"}>{props.children}</main>
   </Aux>
