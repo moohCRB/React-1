4a1874f8010c9c881528efce3f8e74c72a1c1810
IbrahimKoutabli <ibrahim.koutabli@gmail.com>
2018-06-29 16:51:09 +0100

    logic for greying out background in mobile devices if sidebar activated..

--- src/Components/Navigation/SideDrawer/SideDrawer.js
+++ src/Components/Navigation/SideDrawer/SideDrawer.js
@@ -2,16 +2,27 @@ import React from "react";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
 import classes from "./SideDrawer.css"
+import Backdrop from "../../UI/Backdrop/backdrop";
+import Aux from "../../../hoc/Aux";
 
 // The tab that appears on the LHS.
 const sideDrawer = props => {
+    let attachedClasses = [classes.SideDrawer,classes.Close];
+    if (props.open){
+attachedClasses=[classes.SideDrawer,classes.Open]
+    }
   return (
-    <div className={classes.SideDrawer}>
-      <Logo height="11%"/>
-      <nav>
+      <Aux>
+      <Backdrop show={props.open} clicked={props.closed}/>
+    <div className={attachedClasses.join(' ')}>
+    <div className={classes.Logo}>
+      LOGO
+      </div>
+      <nav className={classes.DesktopOnly}>
         <NavigationItems />
       </nav>
     </div>
+    </Aux>
   );
 };
 
