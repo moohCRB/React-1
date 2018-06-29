4887919f502e2486d85409ca7e0e22bad1196e8e
IbrahimKoutabli <ibrahim.koutabli@gmail.com>
2018-06-29 12:00:08 +0100

    Comment on backdrop

--- src/Components/UI/Backdrop/backdrop.js
+++ src/Components/UI/Backdrop/backdrop.js
@@ -1,6 +1,7 @@
 import React from "react";
 import classes from "./backdrop.css";
 
+// This where you can click on the background and close things like tabs/modal
 const backdrop = (props)=> (
     props.show ? <div className={classes.Backdrop} onClick={props.clicked}></div> : null
 );
