f2220cd0647d5e70f60e568ab425f8fc11f9cc0c
IbrahimKoutabli <ibrahim.koutabli@gmail.com>
2018-07-02 09:54:42 +0100

    Included the AppPosts in the DOM

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Containers/App';
 import registerServiceWorker from './registerServiceWorker';
+import AppPosts from "./Containers/Blog/Blog";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
