2984410356d456dcbd094831b173b7e367065498
IbrahimKoutabli <ibrahim.koutabli@gmail.com>
2018-07-02 09:57:53 +0100

    Removed axios to correctly link dependencies

--- src/Containers/Blog/Blog.js
+++ src/Containers/Blog/Blog.js
@@ -1,20 +1,19 @@
 import React, { Component } from "react";
-import axios from "axios"; // axios for get requests in React
+// import axios from "axios"; // axios for get requests in React
 import FullPost from "../../Components/FullPost/FullPost";
 import Post from "../../Components/Post/Post";
 import NewPost from "../../Components/NewPost/NewPost";
 
 
 class Blog extends Component {
-    componentDidMount(){
-axios.get("https://jsonplaceholder.typicode.com/posts")  // Can't save this to a variable as its
-// asynchronous. Therefore, we have to use promises.
-// Then is a method that takes a function as an argument that will execute when the GET is complete
- .then(response => {
-     console.log(response);
- })
-
-    }
+//     componentDidMount(){
+// axios.get("https://jsonplaceholder.typicode.com/posts")  // Can't save this to a variable as its
+// // asynchronous. Therefore, we have to use promises.
+// // Then is a method that takes a function as an argument that will execute when the GET is complete
+//  .then(response => {
+//      console.log(response);
+//  })
+//     }
 
   render() {
     return (
