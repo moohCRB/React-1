cc183bba543b49e54c96ddfae86129f47177cd74
IbrahimKoutabli <ibrahim.koutabli@gmail.com>
2018-07-02 12:38:54 +0100

    Use axios interceptors for global use

--- src/index.js
+++ src/index.js
@@ -3,7 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Containers/App';
 import registerServiceWorker from './registerServiceWorker';
-import AppPosts from "./Containers/Blog/Blog";
+import AppPosts from "./Containers/App_Posts";
+// import axios from "axios";
+
+// Sets up the defaults for ALL requests being sent
+// axios.defaults.baseURL = "https://example.com"
+// axios.defaults.headers.common['Authorization'] = "AUTH TOKEN";
+// axios.defaults.headers.post["Content-Type"] = "application/josn"
+
+
+
+// global, accessible to all files
+// axios.interceptors.request.use(request => {
+//     console.log(request);
+//     return request // this is a must, if not it will be blocked
+// }, error=>{
+//     console.log(error);
+//     return Promise.reject(error);
+// }); 
+// axios.interceptors.response.use(request => {
+//     console.log(request);
+//     return request // this is a must, if not it will be blocked
+// }, error=>{
+//     console.log(error);
+//     return Promise.reject(error);
+// })
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
