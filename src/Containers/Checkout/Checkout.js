c4e49b6e176f24d43b6da2d33ce3a0e4a93ec65b
IbrahimKoutabli <ibrahim.koutabli@gmail.com>
2018-07-06 12:00:18 +0100

    Take queries from the URL

--- src/Containers/Checkout/Checkout.js
+++ src/Containers/Checkout/Checkout.js
@@ -1,6 +1,8 @@
 import React, {Component} from "react";
+import {Route} from "react-router-dom";
 
 import CheckoutSummary from "../../Components/Order/CheckoutSummary/CheckoutSummary";
+import ContactData from "./ContactData/ContactData";
 
 class Checkout extends Component {
 state={
@@ -11,11 +13,31 @@ state={
         lamb:1
     }
 }
+componentDidMount (){
+    // Takes data from the URL
+    const query = new URLSearchParams(this.props.location.search);
+const ingredients = {};
+for (let param of query.entries()){
+    // ["salad","1"], so transform the array to an object
+    ingredients[param[0]]= +param[1]
+}
+this.setState({ingredients:ingredients});
+}
 
+checkoutCancelledHandler = ()=>{
+this.props.history.goBack();
+}
+checkoutContinuedHandler = () => {
+    this.props.history.replace("/checkout/contact-data");
+}
     render(){
 return (
+ 
     <div>
-        <CheckoutSummary ingredients={this.state.ingredients}/>
+        <CheckoutSummary ingredients={this.state.ingredients}
+        checkoutCancelled={this.checkoutCancelledHandler}
+        checkoutContinue={this.checkoutContinuedHandler}/>
+<Route path={this.props.match.path + "/contact-data"} component={ContactData}/>
         </div>
 )
     }
