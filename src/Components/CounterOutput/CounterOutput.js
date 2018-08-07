7130af58db276a566aa149dbbaf2ab545096fabc
IbrahimKoutabli <ibrahim.koutabli@gmail.com>
2018-08-07 15:01:29 +0100

    Component for the incremental

---
+++ src/Components/CounterOutput/CounterOutput.js
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./CounterOutput.css";
+
+const counterOutput = props => (
+  <div className={classes.CounterOutput}>Current Counter: {props.value}</div>
+);
+
+export default counterOutput;
